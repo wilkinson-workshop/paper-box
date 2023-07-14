ec41f679fe7bfedcd515dcc9abb289ab84661e0d
minecraftd <workshop.mincraftd@gmail.com>
2023-07-14 15:55:10 -0500

    update plugin list

--- tools/install_plugins.sh
+++ tools/install_plugins.sh
@@ -43,10 +43,10 @@ from_source() {
 
 server_common() {
 
-    # World Edit assets \
-    cached_wget_iter \
-        https://dev.bukkit.org/projects/worldedit/files/4445117/download \
-        https://dev.bukkit.org/projects/worldguard/files/4554903/download
+    # WorldEdit and Protections Assets.
+    wget -qN -O WorldEdit-1.19.4.jar https://dev.bukkit.org/projects/worldedit/files/4445117/download
+    wget -qN -O WorldGuard-1.19.4.jar https://dev.bukkit.org/projects/worldguard/files/4554903/download
+    wget -qN -O lwc-1.19.4.jar https://dev.bukkit.org/projects/lwc/files/latest
 
     # EssentialsX assets
     cached_wget_iter \
@@ -88,7 +88,8 @@ server() {
             https://github.com/Nuytemans-Dieter/BetterSleeping/releases/download/v4.0.2/BetterSleeping.jar \
             https://github.com/BlueMap-Minecraft/BlueMap/releases/download/v3.14/BlueMap-3.14-spigot.jar \
             https://github.com/Gecolay/GSit/releases/download/1.4.5/GSit-1.4.5.jar \
-            https://github.com/timbru31/SilkSpawners/releases/download/silkspawners-7.4.0/SilkSpawners.jar
+            https://github.com/timbru31/SilkSpawners/releases/download/silkspawners-7.4.0/SilkSpawners.jar \
+            https://hangarcdn.papermc.io/plugins/ChestShop/ChestShop/versions/3.12.2/PAPER/ChestShop.jar
     }
 
     pushd $TARGET_DIR/plugins
